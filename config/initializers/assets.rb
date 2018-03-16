b33c28ab68b9e143c16abac75518e3956714ce90
Hadi Hafeez <mhadihafeez@gmail.com>
2018-03-16 19:10:17 +0500

    complete html

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( jquery.min.js html5shiv.js jquery.scrollex.min.js jquery.scrolly.min.js main.js respond.min.js skel.min.js util.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
